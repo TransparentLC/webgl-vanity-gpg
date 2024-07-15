45f8571084348efec4dbb4783aa0b1880f92bd69
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2024-07-15 16:25:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '',
     plugins: [
         (() => {
             // Download shader_minifier executable from:
